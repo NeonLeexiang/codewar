6f82e0b6b222060bbfeea24ead9026f21e837b19
Leexiang <NeonLeexiang@users.noreply.github.com>
2018-07-31 10:16:16 +0800

    Create For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre.py

---
+++ For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre.py
@@ -0,0 +1,20 @@
+"""
+statements = {
+    'george saint pierre': "I am not impressed by your performance.",
+    'conor mcgregor': "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+}
+
+def quote(fighter):
+    return statements[fighter.lower()]
+"""
+
+
+def quote(fighter):
+    return "I am not impressed by your performance." if str.lower(fighter)=='george saint pierre' else "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+    
+    
+"""
+def quote(fighter):
+    return ["I am not impressed by your performance.",\
+    "I'd like to take this chance to apologize.. To absolutely NOBODY!"][fighter.lower() == 'conor mcgregor']
+"""
