075fa294fbff3929f358cf2c933a96cd0331f84c
Leexiang <NeonLeexiang@users.noreply.github.com>
2018-08-04 11:50:45 +0800

    Create grassshopper_summation.py

---
+++ 8kyu/grassshopper_summation.py
@@ -0,0 +1,19 @@
+def summation(num):
+    return num+num*(num-1)*0.5
+    
+
+"""
+def summation(num):
+    return sum(xrange(num + 1))
+"""
+
+
+"""
+def summation(num):
+    return sum(range(1,num+1))
+"""
+
+
+"""
+summation=lambda n:n*-~n>>1
+"""
