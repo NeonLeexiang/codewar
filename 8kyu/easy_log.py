5970a259cb01b2c6c7b20ec0c1bffc249a8c2f07
Leexiang <NeonLeexiang@users.noreply.github.com>
2018-08-08 14:22:22 +0800

    Create easy_log.py

---
+++ 8kyu/easy_log.py
@@ -0,0 +1,14 @@
+import math
+
+
+def logs(x, a, b):
+    # Your code here
+    return math.log(a,x)+math.log(b,x)
+    
+    
+"""
+from math import log
+def logs(x, a, b):
+    # Your code here
+    return log(a*b, x)
+"""
