ca4f03d7f8401a63a544e58241f070c22f8a3d9a
Leexiang <NeonLeexiang@users.noreply.github.com>
2018-10-17 15:23:41 +0800

    Create will_you.py

---
+++ 7kyu/will_you.py
@@ -0,0 +1,13 @@
+def will_you(young, beautiful, loved):
+    return (young and beautiful) != loved
+    
+    
+"""
+def will_you(y, b, l): return y&b^l
+"""
+
+
+"""
+def will_you(y, b, l):
+    return y*1+b*2+l*3 > 2 and not y==b==l 
+"""
