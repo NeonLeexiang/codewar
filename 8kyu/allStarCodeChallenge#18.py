f73c577b8fd9e3d046672d098aaf5dfc2a08e8c3
Leexiang <NeonLeexiang@users.noreply.github.com>
2018-10-29 14:09:07 +0800

    Create allStarCodeChallenge#18.py

---
+++ 8kyu/allStarCodeChallenge#18.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+
+def str_count(string, letter):
+    return Counter(string)[letter]
+
+
+"""
+def strCount(string, letter):
+    return string.count(letter)
+"""
+
+
+"""
+strCount = str.count
+"""
+
+
+"""
+str_count = lambda strng, letter: strng.count(letter)
+"""
